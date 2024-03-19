0d42a83a4f2cf2f9f4ee8c7e5a343e49db3730a9
Leonardo <leonardoc.soares08@gmail.com>
2024-03-19 17:36:28 -0300

    feat: integration api

---
+++ src/components/order-status.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx'
+
+export type OrderStatus =
+  | 'pending'
+  | 'canceled'
+  | 'processing'
+  | 'delivering'
+  | 'delivered'
+
+interface OrderStatusProps {
+  status: OrderStatus
+}
+
+const orderStatusMap: Record<OrderStatus, string> = {
+  pending: 'Pendente',
+  canceled: 'Cancelado',
+  delivered: 'Entregue',
+  delivering: 'Em entrega',
+  processing: 'Em preparo',
+}
+
+export function OrderStatus({ status }: OrderStatusProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <span
+        className={clsx('h-2 w-2 rounded-full', {
+          'bg-slate-400': status === 'pending',
+          'bg-rose-500': status === 'canceled',
+          'bg-emerald-500': status === 'delivered',
+          'bg-amber-500': status === 'delivering' || status === 'processing',
+        })}
+      />
+
+      <span className=" "></span>
+      <span className="font-medium text-muted-foreground">
+        {orderStatusMap[status]}
+      </span>
+    </div>
+  )
+}
