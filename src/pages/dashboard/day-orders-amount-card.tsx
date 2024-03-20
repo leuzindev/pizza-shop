613448e102c8bc9444bc7449bce757d4eafd4cd3
Leonardo <leonardoc.soares08@gmail.com>
2024-03-20 08:34:34 -0300

    style: ui

--- src/pages/dashboard/day-orders-amount-card.tsx
+++ src/pages/dashboard/day-orders-amount-card.tsx
@@ -1,47 +1,49 @@
 import { useQuery } from '@tanstack/react-query'
-import { DollarSign } from 'lucide-react'
+import { Utensils } from 'lucide-react'
 
-import { getMonthCanceledOrdersAmount } from '@/api/get-month-canceled-orders-amount'
+import { getDayOrdersAmount } from '@/api/get-day-orders-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
-export function MonthCanceledOrdersAmountCard() {
-  const { data: monthCanceledOrdersAmount } = useQuery({
-    queryKey: ['metrics', 'month-canceled-orders-amount'],
-    queryFn: getMonthCanceledOrdersAmount,
+import { MetricCardSkeleton } from './metric-card-skeleton'
+
+export function DayOrdersAmountCard() {
+  const { data: dayOrdersAmount } = useQuery({
+    queryKey: ['metrics', 'day-orders-amount'],
+    queryFn: getDayOrdersAmount,
   })
 
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-base font-semibold">
-          Cancelamentos (mês)
-        </CardTitle>
-        <DollarSign className="h-4 w-4 text-muted-foreground" />
+        <CardTitle className="text-base font-semibold">Pedidos (dia)</CardTitle>
+        <Utensils className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {monthCanceledOrdersAmount && (
+        {dayOrdersAmount ? (
           <>
             <span className="text-2xl font-bold tracking-tight">
-              {monthCanceledOrdersAmount.amount.toLocaleString('pt-BR')}
+              {dayOrdersAmount.amount.toLocaleString('pt-BR')}
             </span>
             <p className="text-xs text-muted-foreground">
-              {monthCanceledOrdersAmount.diffFromLastMonth < 0 ? (
+              {dayOrdersAmount.diffFromYesterday >= 0 ? (
                 <>
                   <span className="text-emerald-500 dark:text-emerald-400">
-                    {monthCanceledOrdersAmount.diffFromLastMonth}%
+                    +{dayOrdersAmount.diffFromYesterday}%
                   </span>{' '}
-                  em relação ao mês passado
+                  em relação a ontem
                 </>
               ) : (
                 <>
                   <span className="text-rose-500 dark:text-rose-400">
-                    +{monthCanceledOrdersAmount.diffFromLastMonth}%
+                    {dayOrdersAmount.diffFromYesterday}%
                   </span>{' '}
-                  em relação ao mês passado
+                  em relação a ontem
                 </>
               )}
             </p>
           </>
+        ) : (
+          <MetricCardSkeleton />
         )}
       </CardContent>
     </Card>
--- src/routes.tsx
+++ src/routes.tsx
@@ -7,12 +7,13 @@ import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 import { Dashboard } from './pages/dashboard/dashboard'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
@@ -37,5 +38,9 @@ export const router = createBrowserRouter([
         element: <SignUp />,
       },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
